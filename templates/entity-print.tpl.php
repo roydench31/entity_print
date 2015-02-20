90c850992408e377afdd8397aed0bb33d2653bfd
Ben Dougherty <ben@dougherty.net.au>
2015-02-20 17:11:23 +0800

    Added support for per entity type and bundle stylesheets in the PDF's

--- templates/entity-print.tpl.php
+++ templates/entity-print.tpl.php
@@ -7,13 +7,14 @@
  * Available variables are:
  *  - $entity - The entity itself.
  *  - $entity_array - The renderable array of this entity.
+ *  - $entity_print_css - An array of stylesheets to be rendered.
  */
 ?>
 
 <html>
 <head>
   <title>PDF</title>
-  <?php print drupal_get_css();?>
+  <?php print drupal_get_css($entity_print_css);?>
 </head>
 <body>
 <div class="page">
