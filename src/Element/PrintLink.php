75320f1626730eef9828af9824c11b8a706621cb
Ben Dougherty <ben@dougherty.net.au>
2016-10-06 15:10:22 +0800

    Issue #2810867 by benjy, dotist, Sam152: Template for "View PDF" link

--- src/Element/PrintLink.php
+++ src/Element/PrintLink.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\entity_print\Element;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Render\Element\Link;
 
 /**
@@ -32,7 +33,7 @@ class PrintLink extends Link {
   public static function preRenderLink($element) {
     if (isset($element['#export_type'])) {
       $element['#attributes']['class'][] = 'print__link--' . $element['#export_type'];
-      $element['#theme_wrappers']['container']['#attributes']['class'][] = 'print__wrapper--' . $element['#export_type'];
+      $element['#theme_wrappers']['container']['#attributes']['class'][] = 'print__wrapper--' . Html::cleanCssIdentifier($element['#export_type']);
     }
 
     return parent::preRenderLink($element);
